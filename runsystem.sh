e2cdc78459b6b34b8fafc3cf7036c798a04ae58a
msamsude <msamsude@sfu.ca>
2015-04-10 16:02:07 -0700

    Removed files to follow group process

--- runsystem.sh
+++
@@ -1,8 +0,0 @@
-ZOOKEEPER_HOST      = $1
-SQS_IN              = $2
-SQS_OUT             = $3
-WRITE_CAP           = $4
-READ_CAP            = $5
-INSTANCES           = $6
-PROXIED_INSTANCES   = $7
-BASE_PORT           = $8
