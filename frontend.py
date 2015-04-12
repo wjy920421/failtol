a18a796298fed2171e7c71e45d63ad6a890f93e2
msamsude <msamsude@sfu.ca>
2015-04-12 16:40:41 -0700

    Fixed major bugs and completed unit testing

--- frontend.py
+++ frontend.py
@@ -4,59 +4,85 @@
   Front end Web server for final assignment, CMPT 474.
 '''
 
-# Library packages
+
 import os
 import sys
 import json
 import os.path
-import utils
 
-# Installed packages
 import boto.sqs
 import boto.sqs.message
-
 from boto.sqs.message import Message
+
+from bottle import Bottle
 from bottle import route, run, request, response, abort, default_app
+
+
+from bottle import run, error, route
 
 AWS_REGION = "us-west-2"
 MAX_SECONDS = 180
 PORT = 8080
 
-global QUEUE_IN = test#sys.argv[1]   ***NEED TO FIX THIS!!!*** set_aws_envkeys file does not pass our arguments. Temp fix hardcoded queue name
+QUEUE_IN = "djWang"#sys.argv[1]
 
 conn = boto.sqs.connect_to_region(AWS_REGION)
-    if conn == None:
-        sys.stderr.write("Could not connect to AWS region '{0}'\n".format(AWS_REGION))
-        sys.exit(1)
+
+if conn == None:
+    sys.stderr.write("Could not connect to AWS region '{0}'\n".format(AWS_REGION))
+    sys.exit(1)
 
 def _response():
     return{
         "data" :{
             "type": "Notification",
             "msg" :  "Accepted"
+            }
         }
-    }
-def delete():
-    userID = request.query.get('id')
-    username = request.query.get('name')
 
-    req = {'data':{
-            'type'       : 'person',
-            'id'         : userID,
-            'name'       : username,
-            }}
+"""
+Delete REST API
+"""
+@route('/delete')
+def delete():
+    userID = request.query.get("id")
+    username = request.query.get("name")
+    
+    data = {}
+    data["type"] = 'person'
+    data["id"]   = userID
+
+    if not username:
+        data["name"] = username
+
+    request_json = {"data" : data}
 
     req = json.dumps(request_json)
-    print _response()
+    m = Message()
+    m.set_body(req)
 
     my_queue = conn.get_queue(QUEUE_IN)
-    m = boto.sqs.message.Message()
-    m.set_body(req)
     my_queue.write(m)
 
     response.status = 202
-    return _response
+    return _response()
+
+"""
+Create SQS connection
+"""
+try:
+    conn = boto.sqs.connect_to_region(AWS_REGION)
+    if conn == None:
+        sys.stderr.write("Could not connect to AWS region '{0}'\n".format(AWS_REGION))
+        sys.exit(1)
+
+    conn.create_queue(QUEUE_IN, MAX_SECONDS)
     
+except Exception as e:
+    sys.stderr.write("Exception connecting to SQS\n")
+    sys.stderr.write(str(e))
+    sys.exit(1)
+
 
 app = default_app()
 run(app, host="localhost", port=PORT)
