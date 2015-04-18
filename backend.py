4836778cccf842c8b76549bb032c11168ee58ca5
msamsude <msamsude@sfu.ca>
2015-04-17 20:34:41 -0700

    Completed basic backend requirements. Used constants from utils file

--- backend.py
+++ backend.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 '''
-  Back end server for final assignment, CMPT 474.
+  Back end server for final assignment CMPT 474.
 '''
 
 # Library packages
-import time
-#import json
+import json
 
 # Installed packages
 import boto.sqs
 
 from bottle import route, run, request, response, default_app
 
-import utils
+import config
 
 QUEUE_OUT = "ex6_out"
+MAX_WAIT_S = 20 # SQS sets max. of 20 s
+
 
 try:
     conn = boto.sqs.connect_to_region(config.AWS_REGION)
@@ -23,37 +24,39 @@ try:
         sys.stderr.write("Could not connect to AWS region '{0}'\n".format(config.AWS_REGION))
         sys.exit(1)
 
-    queue = conn.create_queue(QUEUE_OUT)
+    '''
+      EXTEND:
+      Add code to open the output queue.
+    '''
+    conn.create_queue(QUEUE_OUT)
 
 except Exception as e:
     sys.stderr.write("Exception connecting to SQS\n")
     sys.stderr.write(str(e))
     sys.exit(1)
 
-def retrieve_message():
-    queue = conn.get_queue(QUEUE_OUT)
-    messages = queue.get_messages()
-    if len(messages) == 0:
-        return None
-    message = messages[0]
-    message_json = message.get_body()
-    queue.delete_message(message)
-    return message_json
-
 @route('/')
 def app():
-    message_json = retrieve_message()
+    '''
+      EXTEND:
+      Add code to read a message from the output queue into `m`.
+      Put the message body in `resp`.
+    '''
+    q1 = conn.get_queue(QUEUE_OUT)
+    m = q1.get_messages()
+    m_read = m[0]
+    m_read2 = m_read.get_body()
 
-    if message_json is None:
-        time.sleep(config.MAX_WAIT_S_BACK)
-        message_json = retrieve_message()
-        if message_json is None:
-            response.status = 204 # "No content"
-            return ''
-        else:
-            return message_json
+
+    #m = {'id': 0, 'f': 10, 's': 10, 'actual_s': 5}
+    if m == None:
+        response.status = 204 # "No content"
+        return ''
     else:
-        return message_json
+      q1.delete_message(m_read)
+      resp = json.loads(m_read2)
+      #resp = {'id': 0, 'f': 10, 's': 10, 'actual_s': 5}
+      return resp
 
 app = default_app()
-run(app, host=config.DEFAULT_SUBSCRIBE_TO , port=config.PORT_BACK)
+run(app, host=config.DEFAULT_SUBSCRIBE_TO, port=config.PORT_BACK)
